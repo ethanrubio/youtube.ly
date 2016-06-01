b88d7f7a0a9cca1419a11f58688e4b8c1e675456
Ethan Rubio <ethanrubio@gmail.com>
2016-05-31 20:19:15 -0700

    [chore] adds api key

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+
+const API_KEY = 'AIzaSyDg45gwJn3yD8gPdIOnsLu6DW2wfJzPXnc';
  
 ReactDOM.render(<App />, document.getElementById('app'));
